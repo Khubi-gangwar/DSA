8aeec4dfa357c73af84f24bc742fc48bff619435
Khubi Gangwar <152393785+Khubi-gangwar@users.noreply.github.com>
2026-06-13 17:31:06 +0530

    Time: 30 ms (16.52%) | Memory: 102.6 MB (5.11%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,37 +1,44 @@
 class MinStack {
-    Stack<Integer> st = new Stack<>();
-    Stack<Integer> minSt = new Stack<>();
+    Stack<Long> st = new Stack<>();
+    long min = -1;
     public MinStack() {
         
     }
     
     public void push(int value) {
-        if(st.size() == 0 && minSt.size() == 0) {
-            st.push(value);
-            minSt.push(value);
-        } else {
-            st.push(value);
-            if(minSt.peek() > value) {
-                minSt.push(value);
-            } else {
-                 minSt.push(minSt.peek());
-            }
+        long x = (long)value;
+        if(st.size() == 0) {
+            st.push(x);
+            min = x;
         }
+        else if(x < min) {
+            st.push(2*x-min);
+            min = x;
+        } else if(x >= min)
+         st.push(x);
     }
     
     public void pop() {
-        // if(st.size() > 0 && minSt.size() > 0) {
-        st.pop();
-        minSt.pop();
-        // }
+        if(st.peek() > min) {
+            st.pop();
+        } else {
+            long oldmin = 2*min - st.peek();
+            min = oldmin;
+            st.pop();
+        }      
     }
     
     public int top() {
-        return st.peek();
+        if(st.size() == 0) return -1;
+        long q = st.peek();
+        if(q > min) {
+            return (int)q;
+        } else 
+         return (int)min;
     }
     
     public int getMin() {
-        return minSt.peek();
+        return (int)min;
     }
 }
 
