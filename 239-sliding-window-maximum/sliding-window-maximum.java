b9f0f6a05f211213b6d2c5b34022088410b98490
Khubi Gangwar <152393785+Khubi-gangwar@users.noreply.github.com>
2026-06-15 19:16:44 +0530

    Time: 1648 ms (5.01%) | Memory: 149.9 MB (39.73%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int ans[] = new int[n-k+1];
+        int z = 0;
+        int nse[] = new int[n];
+        Stack<Integer> st = new Stack<>();
+         nse[n-1] = n;
+         st.push(n-1);
+         for(int i = n-2; i >= 0; i--) {
+            while(st.size() > 0 && nums[st.peek()] <= nums[i]) {
+                st.pop();
+            }
+            if(st.size() == 0) nse[i] = n;
+            else nse[i] = st.peek();
+            st.push(i);
+         }
+         for(int i = 0; i < n-k+1; i++) {
+            int j = i;
+            int max = nums[j];
+            while(j < i + k)  {
+                max = nums[j];
+                j = nse[j];
+            }
+              ans[z++] = max;
+         }
+         return ans;
+    }
+}
