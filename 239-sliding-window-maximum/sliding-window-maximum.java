b7782cf49fa5d449379126290ff4201bc88b7274
Khubi Gangwar <152393785+Khubi-gangwar@users.noreply.github.com>
2026-06-16 13:02:49 +0530

    Time: 616 ms (5.00%) | Memory: 149.9 MB (40.32%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -5,26 +5,28 @@ class Solution {
         int z = 0;
         int nse[] = new int[n];
         Stack<Integer> st = new Stack<>();
-         nse[n-1] = n;
-         st.push(n-1);
-         for(int i = n-2; i >= 0; i--) {
-            while(st.size() > 0 && nums[st.peek()] <= nums[i]) {
-                st.pop();
-            }
-            if(st.size() == 0) nse[i] = n;
-            else nse[i] = st.peek();
+        nse[n-1] = n;
+        st.push(n-1);
+        for(int i = n-2; i >= 0; i--) {
+        while(st.size() > 0 && nums[st.peek()] <= nums[i]) {
+            st.pop();
+        }
+        if(st.size() == 0) nse[i] = n;
+          else 
+            nse[i] = st.peek();
             st.push(i);
-         }
-         int j = 0;
-         for(int i = 0; i < n-k+1; i++) {
-            if(j >= i+k) j = i;
+        }
+
+        int j = 0;
+        for(int i = 0; i < n-k+1; i++) {
+            if(j >= k+i) j = i; 
             int max = nums[j];
-            while(j < i + k)  {
+            while(j < k + i) {
                 max = nums[j];
                 j = nse[j];
             }
-              ans[z++] = max;
-         }
-         return ans;
+            ans[z++] = max;
+        }
+        return ans;
     }
 }
